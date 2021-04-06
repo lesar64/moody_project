5e2757c129d72b5dcd11a253c8f43284625e8fa6
Nandor Babina <nandor.babina2@porsche.de>
2021-04-06 19:02:51 +0200

    Displaying emotions

--- src/app/components/screen-recorder/screen-recorder.component.ts
+++ src/app/components/screen-recorder/screen-recorder.component.ts
@@ -1,4 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FaceDetectionService } from '../../services/face-detection.service';
+import * as faceapi from 'face-api.js';
 
 @Component({
   selector: 'app-screen-recorder',
@@ -7,29 +10,33 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class ScreenRecorderComponent implements OnInit {
 
-  @ViewChild('video') videoRef: ElementRef;
+  @ViewChild('videoref') videoRef: ElementRef;
 
-  private srcObject;
-
+  public readonly Math = Math;
   public sharing: boolean = false;
+  public faceDetections$?: Observable<faceapi.WithFaceExpressions<{ detection: faceapi.FaceDetection; }>[]>;
 
-  constructor() { }
+  constructor(private faceDetection: FaceDetectionService) {}
 
   ngOnInit(): void { }
 
   public async startRecording(): Promise<void> {
-    this.srcObject = await (<any>navigator.mediaDevices).getDisplayMedia({
+    this.videoRef.nativeElement.srcObject = await (<any>navigator.mediaDevices).getDisplayMedia({
       audio: false,
     });
+
+    if (!this.faceDetections$) {
+      this.faceDetections$ = this.faceDetection.detectAll(this.videoRef.nativeElement);
+    }
 
     this.sharing = true;
   }
 
   public stopRecording(): void {
-    const tracks = this.srcObject.getTracks();
+    const tracks = this.videoRef.nativeElement.srcObject.getTracks();
     tracks.forEach(track => track.stop());
 
-    this.srcObject = null;
+    this.videoRef.nativeElement.srcObject = null;
     this.sharing = false;
   }
 
---
+++ src/app/services/face-detection.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import * as faceapi from 'face-api.js';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FaceDetectionService {
+
+  constructor() {
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/weights');
+    faceapi.nets.faceExpressionNet.loadFromUri('/assets/weights');
+    faceapi.nets.faceRecognitionNet.loadFromUri('/assets/weights');
+  }
+
+  public detectAll(input: HTMLVideoElement | HTMLImageElement): Observable<faceapi.WithFaceExpressions<{ detection: faceapi.FaceDetection; }>[]> {
+    return interval(1000).pipe(
+      switchMap(() => {
+        return new Observable<faceapi.WithFaceExpressions<{ detection: faceapi.FaceDetection; }>[]>((subscriber) => {
+          faceapi.detectAllFaces(input).withFaceExpressions().then((detectedFaces) => {
+            subscriber.next(detectedFaces);
+            subscriber.complete();
+            return detectedFaces;
+          })
+        })
+      })
+    );
+  }
+}
