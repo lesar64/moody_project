c4a857d5336591591040b83fc5baca484fc1a535
Nandor Babina <nandor.babina2@porsche.de>
2021-05-06 20:12:56 +0200

    Display moving average

--- src/app/pages/mood-barometer/mood-barometer.component.ts
+++ src/app/pages/mood-barometer/mood-barometer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import { filter, map, take, takeUntil } from 'rxjs/operators';
+import { filter, map, scan, take, takeUntil } from 'rxjs/operators';
 import { ScreenRecorderService } from 'src/app/services/screen-recorder.service';
 
 @Component({
@@ -11,19 +11,32 @@ import { ScreenRecorderService } from 'src/app/services/screen-recorder.service'
 })
 export class MoodBarometerComponent implements OnInit, OnDestroy {
 
+  static MOVING_AVERAGE_NUMBER = 10;
+
   public averageValue$ = this.screenRecorder.faceDetections$.pipe(
     // Calculate score per person
     map((detections) => detections.map((detection) => {
       return (<any>detection).aggregated.positive - (<any>detection).aggregated.negative;
     })),
-    // Calculate mean
-    map((scores) => {
-      let sum = 0;
-      (<number[]>scores).forEach(score => {
-        sum += score;
-      });
-      return sum / (<number[]>scores).length;
-    }),
+
+    // Calculate mean of all people
+    map(arr => arr.reduce((acc, current) => acc + current, 0) / arr.length),
+
+    // Moving values for the next 10 occurences
+    scan((acc, curr) => {
+      if (!curr) { return acc; }
+
+      acc.push(curr);
+
+      if (acc.length > MoodBarometerComponent.MOVING_AVERAGE_NUMBER) {
+        acc.shift();
+      }
+
+      return acc;
+    }, []),
+
+    // Calculate moving average
+    map(arr => arr.reduce((acc, current) => acc + current, 0) / arr.length),
   );
 
   private ngUnsubscribe: Subject<boolean> = new Subject()
@@ -46,7 +59,7 @@ export class MoodBarometerComponent implements OnInit, OnDestroy {
   }
 
   private onFinishRecording(): void {
-    // this.router.navigateByUrl('analytics');
+    this.router.navigateByUrl('analytics');
   }
 
 }
