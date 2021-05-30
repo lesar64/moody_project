3285aaffeb860edd80eb8cde65beeece51e6d16a
Steffen Weisshaar <steffenweisshaar@yahoo.de>
2021-05-30 14:02:25 +0200

    Added dependencies for ngx-charts and created new pie-chart component

---
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.scss']
+})
+export class PieChartComponent implements OnInit {
+
+  single: any[];
+  view: any[] = [700, 400];
+
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = true;
+  legendPosition: string = 'below';
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  ngOnInit(): void {
+  }
+
+  constructor() {
+    // Object.assign(this, { single });
+  }
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+}
