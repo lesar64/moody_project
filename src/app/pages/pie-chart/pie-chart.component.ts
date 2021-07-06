08ea8f3d5e14f6a66a853cb5ca797f3dab4b2d7d
Steffen Weisshaar <steffenweisshaar@yahoo.de>
2021-07-06 14:15:34 +0200

    Console log commented out

--- src/app/pages/pie-chart/pie-chart.component.ts
+++ src/app/pages/pie-chart/pie-chart.component.ts
@@ -108,15 +108,15 @@ export class PieChartComponent implements OnInit {
   }
 
   onSelect(data): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+    // console.log('Item clicked', JSON.parse(JSON.stringify(data)));
   }
 
   onActivate(data): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
+    // console.log('Activate', JSON.parse(JSON.stringify(data)));
   }
 
   onDeactivate(data): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+    // console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
